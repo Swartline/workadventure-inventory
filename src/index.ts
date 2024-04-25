c32e5d0afd78a7b7361027f16f2baf3a8d747d69
fred <frederic.lopez9@hotmail.com>
2024-04-25 15:03:12 +0200

    add inventory iframe

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { type Item } from './inventory/item.js';
+import { type Item } from "./inventory/item.js";
 
-export * from './inventory/index.js';
+export * from "./inventory/init.js";
+
+export * from "./inventory/index.js";
 export type { Item };
